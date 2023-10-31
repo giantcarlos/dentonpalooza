36460d19ac901a7b291477fadcbe856c133ef2a3
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-10-30 20:26:37 -0500

    site reduction post-event

--- src/App.js
+++ src/App.js
@@ -5,92 +5,24 @@ function App() {
     <>
       <header>
         <p>DENTONPALOOZA 3</p>
-        <h2>SATURDAY / OCTOBER 28, 2023</h2>
-        <hr />
-        <h3>RUBBER GLOVES REAHEARSAL STUDIOS</h3>
       </header>
-      <div className="image-grid">
-          <a href='http://recycledbooks.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6F5XWoE.png"} alt='logo of Recycled Books' /></a>
-          <a href='https://www.morefuncomicsandgames.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/k8Vheyt.png"} alt='logo of More Fun Comics and Games' /></a>
-          <a href='https://orbitpressusa.com/' target="_blank"><img src={"https://i.imgur.com/FzRrdWC.png"} alt='logo of Orbit' /></a>
-          <a href='https://www.littleguys.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/24seD6Q.png"} alt='logo of Little Guys Movers' /></a>
-          <a href='https://www.barnhartsbarbershop.com/' className='image-gray' target="_blank"><img src={"https://i.imgur.com/nJXwjip.png"} alt='logo of Barnharts Barbershop' /></a>
-          <a href='https://www.tenonecheese.com/' className='image-reverse-gray-bright' target="_blank"><img src={"https://static.wixstatic.com/media/8b7170_ee1b95bd9d0047f09d70ec2a56418d82~mv2_d_1962_2025_s_2.png/v1/crop/x_311,y_347,w_1341,h_1328/fill/w_382,h_380,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/10-1_NW_2.png"} alt='logo of Ten One Cheese' /></a>
-          <a href='https://oakstreetdrafthouse.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/L1Jf7L6.png"} alt='logo of Oak Street Drafthouse' /></a>
-          <a href='https://www.eastsidedtx.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/CWrmgQg.jpg"} alt='logo of Eastside' /></a>
-          <a href='https://www.missangelinesdenton.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/GBB5S1l.png"} alt='logo of Miss Angelines' /></a>
-          <a href='https://rubberglovesdenton.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6ZhmD7H.png"} alt='logo of Rubber Gloves' /></a>
-          <a href='https://danssilverleaf.com/' target="_blank"><img src={"https://i.imgur.com/jHoY46F.png"} alt='logo of Dans Silverleaf' /></a>
-          <div className="message">A big thanks to our sponsors!</div>
-      </div>
       <div className="container">
         <div className="card">
-          <div className="card-title">TICKET INFORMATION</div>
-          <div className="image-container">
-            <img src={"/images/jaffe.jpg"} alt='image Sarah Jaffe' />
-            <img src={"/images/deepblue.jpg"} alt='image Deep Blue Something' />
-            <img src={"/images/bravecombo.jpg"} alt='image from Brave Combo' />
-            <img src={"/images/tigerhead.jpg"} alt='image of Tigerhead Guy' />
-            <img src={"/images/wrestling.jpg"} alt='image wrestling' />
-            <img src={"/images/costumes.jpg"} className="image-gray" alt='image of Krodent' />
-            <img src={"/images/bull.jpg"} alt='image of bull riding' />
-            <img src={"/images/jaffe.jpg"} alt='image Sarah Jaffe' />
-          </div>
-          <div className="card-details">Each ticket is priced at $20.</div>
-          <div className="card-details">Funds raised will benefit both&nbsp; 
-            <a href="https://leukemiatexas.org/" target="_blank">Leukemia Texas</a> and the&nbsp; 
-            <a href="https://dmacdenton.org/" target="_blank">DMAC</a>.
-          </div>
-          <a href='https://www.prekindle.com/event/33981-dentonpalooza-3-denton' target="_blank">
-            <button className="button">BUY NOW</button>
-          </a>
+          <div className="card-title">THANK YOU DENTON, TX!</div>
+          <div className="card-details">Thank you for a successful DENTONPALOOZA 3. Over $4k was raised, putting our lifetime contributions to charity over $26k. See you next year!</div>
         </div>
         <div className="card">
-          <div className="card-title">LINEUP & SCHEDULE</div>
-          <div className="schedule-image">
-            <img src={"/images/flyer.png"} alt='image of the Flyer' />
-          </div>
-          <div className="schedule">
-            <div className="card-details">4:00pm -- Doors Open</div>
-            <div className="card-details">4:30pm -- Sidewalk Chalk Art Expo +&nbsp;
-            <a href='https://www.facebook.com/DJJustrill/' target="_blank">DJ Ju$trill</a></div>
-            <div className="card-details">6:00pm --&nbsp;
-            <a href='https://bluberrytuesday.bandcamp.com/' target="_blank">Blüberry Tüesday</a></div>
-            <div className="card-details">7:00pm --&nbsp;
-            <a href='https://www.instagram.com/hesagoodgary/' target="_blank">Gary</a>: Dog Mayor Re-Election Kickoff</div>
-            <div className="card-details">7:15pm --&nbsp;
-            <a href='https://linktr.ee/smotheredband' target="_blank">Smothered</a></div>
-            <div className="card-details">8:10pm -- Protesting with Fred Flintsone</div>
-            <div className="card-details">8:15pm -- Do You Know Your Cheese with&nbsp;
-            <a href='https://www.instagram.com/lanky_likes_food/' target="_blank">Secret Chef Lanky</a></div>
-            <div className="card-details">8:45pm -- The Ghost of Festivals Past - T-Shirt Cannon</div>
-            <div className="card-details">9:00pm --&nbsp;
-            <a href='https://doforitrecords.com/shaolin-death-squad/' target="_blank">Shaolin Death Squad</a></div>
-            <div className="card-details">10:15pm -- $666 Costume Contest</div>
-            <div className="card-details">11:30pm -- The Ghost of Festivals Past - T-Shirt Cannon</div>
-            <div className="card-details">11:45pm --&nbsp;
-            <a href='https://sarahjaffe.com/' target="_blank">Sarah Jaffe</a></div>
-            <div className="card-details">1:30am -- End of Show</div>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-title">MERCH STORE</div>
-          <div className="merch-image">
-            <img src={"/images/slap.png"} alt='image of this years shirt design' />
-          </div>
-          <div className="merch">
-            <div className="card-details">The public has spoken.</div>
-            <div className="card-details">We are now selling Dentonpalooza 3 t-shirts, courtesy of our friends at&nbsp; 
-              <a href="http://www.slapdenton.com/" target="_blank">SLAP</a>.
+          <div className="card-title">PRESS FOR DENTONPALOOZA 3</div>
+          <a href="https://dentonrc.com/look-dentonpalooza-3-returns-with-more-live-music-and-odder-oddities/article_76a16d00-f08a-5698-af07-8f773598c54e.html" target="_blank"  className="press-card">
+            <div className="press-image">
+              <img src={"https://bloximages.newyork1.vip.townnews.com/dentonrc.com/content/tncms/assets/v3/editorial/2/77/277fb49b-2ea6-5b02-9b14-5b536a5e88cb/653f2441223f2.image.jpg?resize=750%2C500"} className="image-gray" alt='image of Smothered' />
             </div>
-            <div className="card-details">Shirts are available in black, ash white, and navy blue.</div>
-            <a href='https://311d51078989303.s4shops.com/' target="_blank">
-              <button className="button">BUY NOW</button>
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-title">PRESS & BLURBS</div>
+            <div className="press">
+            <div className="card-details">October 30th, 2023</div>
+              <div className="press-title">Dentonpalooza 3 Returns with More Live Music and Odder Oddities</div>
+              <div className="card-details">Juan Betancourt / Denton Record-Chronicle</div>
+            </div>
+          </a>
           <a href="https://www.dallasnews.com/opinion/commentary/2023/10/27/dentonpalooza-embraces-halloween/?outputType=amp" target="_blank"  className="press-card">
             <div className="press-image">
               <img src={"/images/dmn.avif"} className="image-gray" alt='image of Dentonpalooza from the Dallas Morning News' />
